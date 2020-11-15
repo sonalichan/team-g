e5bf209f97b76098e0b3c3f7213d5c20add58232
nvon <32344564+nvon@users.noreply.github.com>
2020-11-14 20:12:12 -0800

    Update CalendarPage.js

--- gerbil-calendar/src/Components/CalendarPage.js
+++ gerbil-calendar/src/Components/CalendarPage.js
@@ -5,18 +5,23 @@ export class CalendarPage extends Component {
     componentDidMount() {
       window.scrollTo(0, 0);
     }
+
+    /* 
+
+    class Calendar {
+      const calendar = document.querySelector("#app-calendar");
+      for (let day = 1; day <= 7; day = day++) {
+        console.log(day)
+        calendar.insertAdjacentHTML("beforeend", '<div class="day">${day}</div>')
+      }
+    } */
+    
   
     render() {
+      /* */
       return (
         <div id="homePage">
           <div className="gerbil-message">
-            <p>Hi, I am Gerbil, a social animal who loves human-beings!</p>
-
-            <p>I have travelled to a lot of places and people love me. (except Californians as I ate too of their agriculture... Sorry:P</p>
-
-            <p>I hope I could get to know more about you and your life! Come and say “Hi!” once in a while to update me what you have been or will be doing!! </p>
-
-            <p>See you around!</p>
           </div>
           <div className="gerbil-img">
               <img src="/img/gerbil-image.png" alt="a gerbil's picture"/>
@@ -25,4 +30,3 @@ export class CalendarPage extends Component {
       );
     }
   }
-  
